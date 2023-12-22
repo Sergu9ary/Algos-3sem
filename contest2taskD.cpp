072c4e7511820bc247ccb144e9cf8c4d92a42f9c
Sergu9ary <73709042+Sergu9ary@users.noreply.github.com>
2023-12-22 23:32:36 +0300

    Update contest2taskD.cpp

--- contest2taskD.cpp
+++ contest2taskD.cpp
@@ -4,21 +4,20 @@
 
 void DFSGraph(size_t vertex, const std::vector<std::vector<size_t>>& graph, std::vector<bool>& used, std::stack<size_t>& order) {
     used[vertex] = true;
-    for(auto& neighbour: graph[vertex]) {
-        if(!used[neighbour]) {
+    for (auto& neighbour : graph[vertex]) {
+        if (!used[neighbour]) {
             DFSGraph(neighbour, graph, used, order);
         }
     }
     order.push(vertex);
 }
 
-
-void DFSTrGraph(size_t vertex, const std::vector<std::vector<size_t>>& graph_t, std::vector<bool>& used, std::vector<size_t>& components, size_t current_component) {
+void DFSExploreComponent(size_t vertex, const std::vector<std::vector<size_t>>& graph_t, std::vector<bool>& used, std::vector<size_t>& components, size_t current_component) {
     components[vertex] = current_component;
     used[vertex] = true;
-    for(auto& i: graph_t[vertex]) {
-        if(!used[i]) {
-            DFSTrGraph(i, graph_t, used, components, current_component);
+    for (auto& i : graph_t[vertex]) {
+        if (!used[i]) {
+            DFSExploreComponent(i, graph_t, used, components, current_component);
         }
     }
 }
@@ -26,18 +25,19 @@ void DFSTrGraph(size_t vertex, const std::vector<std::vector<size_t>>& graph_t, 
 std::vector<size_t> Kosaraju(const std::vector<std::vector<size_t>>& graph, const std::vector<std::vector<size_t>>& graph_t, size_t& size) {
     std::vector<bool> used(graph.size(), false);
     std::stack<size_t> order;
-    for(size_t i = 0; i < graph.size(); ++i) {
-        if(!used[i]) {
+    for (size_t i = 0; i < graph.size(); ++i) {
+        if (!used[i]) {
             DFSGraph(i, graph, used, order);
         }
     }
     used.assign(graph.size(), false);
     std::vector<size_t> components(graph.size());
     size_t component = 1;
-    for(; !order.empty(); order.pop()) {
+    while (!order.empty()) {
         size_t vertex = order.top();
-        if(!used[vertex]) {
-            DFSTrGraph(vertex, graph_t, used, components, component);
+        order.pop();
+        if (!used[vertex]) {
+            DFSExploreComponent(vertex, graph_t, used, components, component);
             component++;
         }
     }
@@ -45,24 +45,34 @@ std::vector<size_t> Kosaraju(const std::vector<std::vector<size_t>>& graph, cons
     return components;
 }
 
-
-int main() {
-    size_t n, m;
-    std:: cin >> n >> m;
-    std::vector<std::vector<size_t>> graph(n);
-    std::vector<std::vector<size_t>> graph_t(n);
+void getInput(std::istream& in, size_t& n, size_t& m, std::vector<std::vector<size_t>>& graph, std::vector<std::vector<size_t>>& graph_t) {
+    in >> n >> m;
+    graph.resize(n);
+    graph_t.resize(n);
     size_t a, b;
-    for(size_t i = 0; i < m; ++i) {
-        std::cin >> a >> b;
+    for (size_t i = 0; i < m; ++i) {
+        in >> a >> b;
         graph[a - 1].push_back(b - 1);
         graph_t[b - 1].push_back(a - 1);
     }
-    size_t size;
-    std::vector<size_t> result = Kosaraju(graph, graph_t, size);
-    std:: cout << size;
-    std:: cout << std::endl;
-    for(auto& i : result) {
-        std:: cout << i;
-        std:: cout << ' ';
+}
+
+void displayResult(std::ostream& out, const std::vector<size_t>& result, size_t size) {
+    out << size << std::endl;
+    for (auto& i : result) {
+        out << i << ' ';
     }
+    out << std::endl;
+}
+
+int main() {
+    size_t n, m, size;
+    std::vector<std::vector<size_t>> graph;
+    std::vector<std::vector<size_t>> graph_t;
+    getInput(std::cin, n, m, graph, graph_t);
+
+    std::vector<size_t> result = Kosaraju(graph, graph_t, size);
+    displayResult(std::cout, result, size);
+
+    return 0;
 }
